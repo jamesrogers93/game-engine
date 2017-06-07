0b80e1a286a7e6452f78fb6b8092e5dc126f2d95
James Rogers <jamesrogers93@hotmail.co.uk>
2017-06-07 17:31:52 +0100

    macOS version specified with glm and opengl libraries linked in CMakeLists.txt

--- include/Defines/OpenGL.h
+++ include/Defines/OpenGL.h
@@ -4,7 +4,12 @@
 #ifdef __ANDROID__
 #include <GLES3/gl3.h>
 #include <EGL/egl.h>
-#elif __APPLE__
+
+#elif __iOS__
 #define GL_GLEXT_PROTOTYPES
 #include <OpenGLES/ES3/gl.h>
+
+#elif __macOS__
+#include <OpenGL/gl3.h>
+
 #endif
