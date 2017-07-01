6080f1192e6973b2726d1e38c83fe14aa63e97aa
James Rogers <jamesrogers93@hotmail.co.uk>
2017-07-01 11:30:52 +0100

    Added System that contains info about device

---
+++ game-engine/include/game-engine/Core/Engine/System.h
@@ -0,0 +1,11 @@
+#ifndef _SYSTEM_H
+#define _SYSTEM_H
+
+class System
+{
+public:
+    static unsigned int screenWidth;
+    static unsigned int screenHeight;
+};
+
+#endif /* _SYSTEM_H */
---
+++ game-engine/src/Core/Engine/System.cpp
@@ -0,0 +1,4 @@
+#include "game-engine/Core/Engine/System.h"
+
+unsigned int System::screenWidth = 800;
+unsigned int System::screenHeight = 800;
