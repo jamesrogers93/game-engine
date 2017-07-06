60ffa592dd4e42d5021fd6a5e8b174c5f3f94139
James Rogers <jamesrogers93@hotmail.co.uk>
2017-07-06 20:27:45 +0100

    Removed glut from OpenGL.h

--- game-engine/include/game-engine/Defines/OpenGL.h
+++ game-engine/include/game-engine/Defines/OpenGL.h
@@ -10,20 +10,14 @@
     #include <OpenGLES/ES3/gl.h>
     #include <OpenGLES/ES3/glext.h>
 
-#elif __MACOS__
-    #include <OpenGL/gl3.h>
-
 #elif __APPLE__
     #include <OpenGL/gl.h>
     #include <OpenGL/glu.h>
-    #include <GLUT/glut.h>
+
 #else
     #ifdef _WIN32
         #include <windows.h>
     #endif
     #include <GL/gl.h>
     #include <GL/glu.h>
-    #include <GL/glut.h>
-#endif
-
 #endif
