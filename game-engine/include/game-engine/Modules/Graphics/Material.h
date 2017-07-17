0df1080338455b3aa4e6891e02f6dd950a202940
James Rogers <jamesrogers93@hotmail.co.uk>
2017-07-17 12:43:24 +0100

    Added importers

--- game-engine/include/game-engine/Modules/Graphics/Material.h
+++ game-engine/include/game-engine/Modules/Graphics/Material.h
@@ -21,6 +21,8 @@ private:
     static const std::string SHADER_SPEC_SOLID_NAME;
     static const std::string SHADER_SHININESS_NAME;
     
+    std::string name;
+    
     std::string diffuseTexture, specularTexture;
     glm::vec4 diffuseSolid, specularSolid;
     float shininess;
@@ -29,15 +31,15 @@ private:
     bool isDiffuseSolid, isSpecularSolid;
     
 public:
-    Material(const std::string &diffuseTexture, const std::string &specularTexture, const float &shininess = SHININESS_DEFAULT)
+    Material(const std::string &name, const std::string &diffuseTexture, const std::string &specularTexture, const float &shininess = SHININESS_DEFAULT)
     : diffuseTexture(diffuseTexture), specularTexture(specularTexture), shininess(shininess), isDiffuseTexture(true), isSpecularTexture(true), isDiffuseSolid(false), isSpecularSolid(false)
     {}
     
-    Material(const glm::vec4 &diffuseSolid, const glm::vec4 &specularSolid, const float &shininess = SHININESS_DEFAULT)
+    Material(const std::string &name, const glm::vec4 &diffuseSolid, const glm::vec4 &specularSolid, const float &shininess = SHININESS_DEFAULT)
     : diffuseSolid(diffuseSolid), specularSolid(specularSolid), shininess(shininess), isDiffuseTexture(false), isSpecularTexture(false), isDiffuseSolid(true), isSpecularSolid(true)
     {}
     
-    Material(const std::string &diffuseTexture, const std::string &specularTexture, const glm::vec4 &diffuseSolid, const glm::vec4 &specularSolid, const float &shininess = SHININESS_DEFAULT)
+    Material(const std::string &name, const std::string &diffuseTexture, const std::string &specularTexture, const glm::vec4 &diffuseSolid, const glm::vec4 &specularSolid, const float &shininess = SHININESS_DEFAULT)
     : diffuseTexture(diffuseTexture), specularTexture(specularTexture), diffuseSolid(diffuseSolid), specularSolid(specularSolid), shininess(shininess), isDiffuseTexture(true), isSpecularTexture(true), isDiffuseSolid(true), isSpecularSolid(true)
     {}
     
