994eb5a3fbe6927cc50e47f3acb7b0f36d02f432
James Rogers <jamesrogers93@hotmail.co.uk>
2017-07-17 12:49:57 +0100

    Fixed error where name was not being stored in constructor

--- game-engine/include/game-engine/Modules/Graphics/Material.h
+++ game-engine/include/game-engine/Modules/Graphics/Material.h
@@ -32,19 +32,21 @@ private:
     
 public:
     Material(const std::string &name, const std::string &diffuseTexture, const std::string &specularTexture, const float &shininess = SHININESS_DEFAULT)
-    : diffuseTexture(diffuseTexture), specularTexture(specularTexture), shininess(shininess), isDiffuseTexture(true), isSpecularTexture(true), isDiffuseSolid(false), isSpecularSolid(false)
+    : name(name), diffuseTexture(diffuseTexture), specularTexture(specularTexture), shininess(shininess), isDiffuseTexture(true), isSpecularTexture(true), isDiffuseSolid(false), isSpecularSolid(false)
     {}
     
     Material(const std::string &name, const glm::vec4 &diffuseSolid, const glm::vec4 &specularSolid, const float &shininess = SHININESS_DEFAULT)
-    : diffuseSolid(diffuseSolid), specularSolid(specularSolid), shininess(shininess), isDiffuseTexture(false), isSpecularTexture(false), isDiffuseSolid(true), isSpecularSolid(true)
+    : name(name), diffuseSolid(diffuseSolid), specularSolid(specularSolid), shininess(shininess), isDiffuseTexture(false), isSpecularTexture(false), isDiffuseSolid(true), isSpecularSolid(true)
     {}
     
     Material(const std::string &name, const std::string &diffuseTexture, const std::string &specularTexture, const glm::vec4 &diffuseSolid, const glm::vec4 &specularSolid, const float &shininess = SHININESS_DEFAULT)
-    : diffuseTexture(diffuseTexture), specularTexture(specularTexture), diffuseSolid(diffuseSolid), specularSolid(specularSolid), shininess(shininess), isDiffuseTexture(true), isSpecularTexture(true), isDiffuseSolid(true), isSpecularSolid(true)
+    : name(name), diffuseTexture(diffuseTexture), specularTexture(specularTexture), diffuseSolid(diffuseSolid), specularSolid(specularSolid), shininess(shininess), isDiffuseTexture(true), isSpecularTexture(true), isDiffuseSolid(true), isSpecularSolid(true)
     {}
     
     ~Material()
     {}
+    
+    const std::string& getName() const { return name; }
     
     bool loadToShader(Shader *shader);
     
